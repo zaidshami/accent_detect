627868e02daf0bd34c7544ce3bd41c27d6002564
zaidshami <zaidalshami8@gmail.com>
2025-05-30 23:40:42 +0300

    add print

--- utils.py
+++ utils.py
@@ -77,8 +77,10 @@ def classify_accent(transcript, audio_path):
     print('flatten prediction :')
     print(formatted)
 
-    # prediction_ANN_rounded = [np.argmax(i) for i in formatted]
-    prediction_ANN_rounded =np.argmax(formatted)
+    prediction_ANN_rounded = [np.argmax(i) for i in formatted]
+    print('prediction_ANN_rounded :')
+    print(prediction_ANN_rounded)
+
 
     # speaker = ['american', 'welsh', 'telugu', 'bangla', 'australian', 'british', 'odiya',
     #            'indian', 'malayalam']
